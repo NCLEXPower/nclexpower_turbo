11746039a7e54be0c76f0059a7e7bcb99187c676
costales arnel <costalesarnel08@gmail.com>
2024-11-05 23:24:05 +0800

    @86eqmv3bh #Delivery-board-request-Pricing-Management

--- packages/core-library/system/app/internal/blocks/Hub/Product/pricing/PricingFormBlock.tsx
+++ packages/core-library/system/app/internal/blocks/Hub/Product/pricing/PricingFormBlock.tsx
@@ -3,13 +3,16 @@ import { PricingParams } from '../../../../../../../types/types';
 import { PricingForm } from "./PricingForm";
 
 export function PricingFormBlock() {
-  const { businessQueryCreatePricing } = useBusinessQueryContext();
+  const { businessQueryCreatePricing, businessQueryGetAllPricing } =
+    useBusinessQueryContext();
+  const { refetch } = businessQueryGetAllPricing(["getAllPricing"]);
   const { mutateAsync, isLoading } = businessQueryCreatePricing();
   const { executeToast } = useExecuteToast();
 
   async function onSubmit(values: PricingParams) {
     await mutateAsync({ ...values });
     executeToast("Successfully Added", "top-right", false, { type: "success" });
+    refetch();
   }
   return <PricingForm onSubmit={onSubmit} submitLoading={isLoading} />;
 }
