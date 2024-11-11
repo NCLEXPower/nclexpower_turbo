75281dd6579f727b1a4994cfc14a6d91b9046d70
bryan <bry@payloro.com>
2024-11-11 11:31:15 +0800

    @86eqmv28u #Fix unauthenticated sidebar behavior

--- packages/core-library/components/GenericSidebar/Sidebar.tsx
+++ packages/core-library/components/GenericSidebar/Sidebar.tsx
@@ -1,17 +1,19 @@
+/**
+* Property of the NCLEX Power.
+* Reuse as a whole or in part is prohibited without permission.
+* Created by the Software Strategy & Development Division
+*/
 import React from "react";
-import { Button, Drawer, IconButton, SxProps, Theme, Typography } from "@mui/material";
-import { Box, List } from "@mui/material";
+import { Box, List, Drawer } from "@mui/material";
 import { SidebarListButton } from "./SidebarListButton";
 import { SidebarButton } from "./SidebarButton";
-import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import { useRouter } from "../../core";
 import { usePathname } from "next/navigation";
 import { NCLEXBlueLogo } from "../../assets";
 import Image from "next/image";
 import { MenuItems } from "../../api/types";
-import LogoutIcon from "@mui/icons-material/Logout";
 import { WebSidebarStylesType } from "../../types/web-sidebar-styles";
 import useGetProgramList from "../../hooks/useGetProgramList";
+import { IconButton, EvaIcon } from "../../components";
 
 interface SideBarPropsType extends Partial<WebSidebarStylesType> {
   menu: Array<MenuItems>;
@@ -22,6 +24,38 @@ interface SideBarPropsType extends Partial<WebSidebarStylesType> {
   isMobile?: boolean;
   isAuthenticated: boolean;
 }
+
+interface RenderMenuItemsProps extends Partial<WebSidebarStylesType>{
+  menu: Array<MenuItems>;
+  pathname: string;
+  isAuthenticated: boolean;
+}
+
+const RenderMenuItems: React.FC<RenderMenuItemsProps> = ({ menu, pathname, isAuthenticated, listStyles }) => {
+  return (
+    menu.length > 0 &&
+    menu
+      .filter((item, index, self) => self.findIndex((m) => m.id === item.id) === index)
+      .map((navigation, index) => (
+        <Box key={index}>
+          {navigation.children && navigation.children.length > 0 ? (
+            <SidebarListButton
+              navigation={navigation}
+              pathname={pathname}
+              isAuthenticated={isAuthenticated}
+              listStyles={listStyles}
+            />
+          ) : (
+            <SidebarButton
+              navigation={navigation}
+              pathname={pathname}
+              isAuthenticated={isAuthenticated}
+            />
+          )}
+        </Box>
+      ))
+  );
+};
 
 export const Sidebar: React.FC<SideBarPropsType> = ({
   menu,
@@ -78,45 +112,35 @@ export const Sidebar: React.FC<SideBarPropsType> = ({
           borderColor="divider"
           height={70}
         >
-          <Image
-            style={{ width: 150 }}
-            src={NCLEXBlueLogo}
-            alt="NCLEXLogo"
-          />
+          <Image style={{ width: 150 }} src={NCLEXBlueLogo} alt="NCLEXLogo" />
           <Box position="absolute" right={5}>
-            <IconButton onClick={setOpen}>
-              <KeyboardArrowLeftIcon
-                sx={{
-                  opacity: open ? 1 : 0,
-                  transform: !open ? "rotate(-180deg)" : "rotate(0)",
-                  transition: "0.2s",
-                }}
+            <IconButton onClick={setOpen} className="outline-none">
+              <EvaIcon
+                id="back-icon"
+                name="arrow-ios-back-outline"
+                width={25}
+                height={25}
+                ariaHidden
+                className={`${open ? 'opacity-100' : 'opacity-0'} ${!open ? '-rotate-180' : 'rotate-0'} transition duration-200`}
               />
             </IconButton>
           </Box>
         </Box>
-        {updatedMenu &&
-          updatedMenu.length > 0 &&
-          updatedMenu
-            .filter((navigation) => !navigation.hide)
-            .map((navigation, index) => (
-              <React.Fragment key={index}>
-                {navigation.children && navigation.children.length > 0 ? (
-                  <SidebarListButton
-                    navigation={navigation}
-                    pathname={pathname}
-                    isAuthenticated={isAuthenticated}
-                    listStyles={listStyles}
-                  />
-                ) : (
-                  <SidebarButton
-                    navigation={navigation}
-                    pathname={pathname}
-                    isAuthenticated={isAuthenticated}
-                  />
-                )}
-              </React.Fragment>
-            ))}
+        {!isAuthenticated ? (
+          <RenderMenuItems
+            menu={menu}
+            pathname={pathname}
+            isAuthenticated={isAuthenticated}
+            listStyles={listStyles}
+          />
+        ) : (
+          <RenderMenuItems
+            menu={updatedMenu.filter((navigation) => !navigation.hide)}
+            pathname={pathname}
+            isAuthenticated={isAuthenticated}
+            listStyles={listStyles}
+          />
+        )}
       </List>
     </Drawer>
   );
