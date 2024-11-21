27cd2651ab83417f1f53ac64ff8a8fa7e23cb6cb
costales arnel <costalesarnel08@gmail.com>
2024-11-21 23:39:11 +0800

    @86eqdaxxn #sample implementation of delete modal

--- packages/core-library/system/app/internal/blocks/Hub/QuestionManagement/QuestionManagementPageBlock.tsx
+++ packages/core-library/system/app/internal/blocks/Hub/QuestionManagement/QuestionManagementPageBlock.tsx
@@ -1,20 +1,37 @@
 import React from "react";
-import { Container, Box, Typography, Chip, Button } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import { useRegularQuestionWizardSteps } from "./steps/useSteps";
-import { RegularQuestionsSteps, RegularQuestionStepProps } from "./steps/types";
-import { RegularQuestionFormType } from "./types";
-import { Alert } from '../../../../../../components';
-import { useModal } from '../../../../../../hooks';
+import { Alert } from "../../../../../../components";
+import { useModal } from "../../../../../../hooks";
+import { useDialogContext } from "../../../../../../contexts";
+import { useAtom } from "jotai";
+import { DeleteConfirmationAtom } from "../../../../../../components/Dialog/DialogFormBlocks/DeleteModal/validation";
 
 export const QuestionManagementPageBlock = () => {
   const saveConfirmationModal = useModal<unknown>();
   const { render } = useRegularQuestionWizardSteps(
-    () => { },
+    () => {},
     saveConfirmationModal
   );
 
+  const { openDialog } = useDialogContext();
+
+  const [deleteAtom, setDeleteAtom] = useAtom(DeleteConfirmationAtom);
+
+  const sample = {
+    id: "randomCharacters",
+    text: "text item",
+    inputText: "",
+  };
+
+  const handleDelete = () => {
+    setDeleteAtom(sample);
+    openDialog("delete-modal", "");
+  };
+
   return (
     <Box>
+      <button onClick={handleDelete}>sample delete</button>
       {/* For improvements, all containers should be placed on one codebase. */}
       <Container>
         <Alert
