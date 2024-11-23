acfb186737116a1d8c0214ecefdfe44a6fb00379
Lizter <lizters74@gmail.com>
2024-11-23 21:21:41 +0800

    @86eqctme8 #Unit testing

--- packages/core-library/tests/hooks/useAccessControl.test.ts
+++ packages/core-library/tests/hooks/useAccessControl.test.ts
@@ -1,7 +1,6 @@
 import { useAccessControl } from "../../hooks";
 import { AccessLevels } from "../../core/utils/permission";
 import { renderHook } from "../common";
-import { useAccessLevel } from "../../contexts/auth/hooks";
 
 jest.mock("../../config", () => ({
   config: { value: jest.fn() },
@@ -10,6 +9,8 @@ jest.mock("../../config", () => ({
 jest.mock("../../contexts/auth/hooks", () => ({
   useAccessLevel: jest.fn(),
 }));
+
+const { useAccessLevel } = require("../../contexts/auth/hooks");
 
 describe("useAccessControl", () => {
   afterEach(() => {
@@ -20,35 +21,32 @@ describe("useAccessControl", () => {
     sessionStorage.clear();
   });
 
-  it("should pass if ADMIN has access to OtherConfigurations", () => {
+  it("should return false if ADMIN has no access to ChooseProductsConfigurations", () => {
     (useAccessLevel as jest.Mock).mockImplementation(() => [
       AccessLevels.ADMIN,
-    ]);
-
-    const { result } = renderHook(() => useAccessControl());
-
-    expect(result.current.hasAccess("OtherConfigurations")).toBe(true);
-  });
-
-  it("should pass if ENCODER has no access to ContentManagementSystemSettings", () => {
-    (useAccessLevel as jest.Mock).mockImplementation(() => [
-      AccessLevels.EDITOR,
-    ]);
-
-    const { result } = renderHook(() => useAccessControl());
-    expect(result.current.hasAccess("ContentManagementSystemSettings")).toBe(
-      false
-    );
-  });
-
-  it("should pass if DEVELOPER has no access to ChooseProductsConfigurations", () => {
-    (useAccessLevel as jest.Mock).mockImplementation(() => [
-      AccessLevels.DEVELOPER,
     ]);
 
     const { result } = renderHook(() => useAccessControl());
     expect(result.current.hasAccess("ChooseProductsConfigurations")).toBe(
       false
     );
+  });
+
+  it("should return true if ENCODER  has access to InAppManagement", () => {
+    (useAccessLevel as jest.Mock).mockImplementation(() => [
+      AccessLevels.EDITOR,
+    ]);
+
+    const { result } = renderHook(() => useAccessControl());
+    expect(result.current.hasAccess("InAppManagement")).toBe(true);
+  });
+
+  it("should return true if DEVELOPER has access to OtherConfigurations", () => {
+    (useAccessLevel as jest.Mock).mockImplementation(() => [
+      AccessLevels.DEVELOPER,
+    ]);
+
+    const { result } = renderHook(() => useAccessControl());
+    expect(result.current.hasAccess("OtherConfigurations")).toBe(true);
   });
 });
