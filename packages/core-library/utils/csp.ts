7eaeda4a28597dc86ded9470ee86b671b71902f6
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-11-26 21:48:13 +0800

    @hotfix #add google.com to csp for recaptcha

--- packages/core-library/utils/csp.ts
+++ packages/core-library/utils/csp.ts
@@ -6,7 +6,7 @@ import { ServerResponse } from "http";
 import { getMaintenanceMode } from "../ssr";
 
 export const generateCSP = (generatedNonce: string): string =>
-  `default-src 'self' *.vercel.app; script-src 'self' 'nonce-${generatedNonce}' 'unsafe-eval' https://js.stripe.com *.vercel.app *.herokuapp.com https://vercel.live ` +
+  `default-src 'self' *.vercel.app; script-src 'self' 'nonce-${generatedNonce}' 'unsafe-eval' https://js.stripe.com *.vercel.app *.herokuapp.com https://vercel.live https://www.google.com ` +
   config.value.STRIPE_URL_JS +
   " " +
   `; form-action 'self'; base-uri 'self'; object-src 'self'; style-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com 'unsafe-inline'; connect-src ` +
@@ -15,9 +15,9 @@ export const generateCSP = (generatedNonce: string): string =>
   config.value.LOCAL_API_URL +
   " " +
   config.value.VERCELURL +
-  " *.vercel.app *.herokuapp.com https://js.stripe.com https://api.ipify.org " +
+  " *.vercel.app *.herokuapp.com https://js.stripe.com https://api.ipify.org https://www.google.com " +
   config.value.STRIPE_URL_JS +
-  ` blob:; img-src 'self' data: blob: webpack:; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' *.vercel.app https://js.stripe.com ` +
+  ` blob:; img-src 'self' data: blob: webpack:; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' *.vercel.app https://js.stripe.com https://vercel.live ` +
   " " +
   config.value.STRIPE_URL_JS +
   ";";
