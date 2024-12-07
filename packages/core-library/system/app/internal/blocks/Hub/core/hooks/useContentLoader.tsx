247a87972276ee4d1d54ea5fc5d00fb8f4ac3589
costalesarnel <costalesarnel08@gmail.com>
2024-12-07 11:09:54 +0800

    @86eq301wd #remove unnecessary file

--- packages/core-library/system/app/internal/blocks/Hub/core/hooks/useContentLoader.tsx
+++
@@ -1,6 +0,0 @@
-import { usePageLoaderContext } from "../../../../../../../contexts/PageLoaderContext";
-
-export const useContentLoader = () => {
-  const { contentLoader, setContentLoader } = usePageLoaderContext();
-  return { contentLoader, setContentLoader };
-};
