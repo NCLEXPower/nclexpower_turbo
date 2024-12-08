633d8264aa9c189a92f038898f4cb450ce0d906b
Bryan Palad <bryan.palad@allied.marketing>
2024-12-08 21:09:06 +0800

    @86eqrqnh7 #Billing Summary UI

--- apps/web-customer/src/components/blocks/HubBlocks/BillingSummaryBlock/BillingSummaryBlock.tsx
+++ apps/web-customer/src/components/blocks/HubBlocks/BillingSummaryBlock/BillingSummaryBlock.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { billlingSummaryMockData as data} from "../../../../core/constant/BillingSummaryMock/BillingSummaryMock"; // remove once real data fetching is applied
+import { billlingSummaryMockData as data } from "../../../../core/constant/BillingSummaryMock/BillingSummaryMock"; // remove once real data fetching is applied
 import { DataGrid } from "core-library/components";
 
 import { OrderSummaryBlock, OrderDetailsBlock } from "./index";
@@ -41,20 +41,27 @@ const tableStyle = {
   boxShadow: "0px 10px 60px 0px rgba(226, 236, 249, 0.50)",
   borderRadius: "8px",
   overflow: "hidden",
-  '.MuiDataGrid-columnSeparator': {
-    display: 'none',
+  marginX: "auto",
+  ".MuiDataGrid-columnSeparator": {
+    display: "none",
   },
   ".MuiDataGrid-cell": {
-    cursor: "pointer"
+    cursor: "pointer",
   },
   "&.MuiDataGrid-root .MuiDataGrid-cell:focus-within": {
     outline: "none !important",
   },
-  '& .super-app-theme--header': {
-     background: "rgba(24, 30, 47, 0.95)",
-     outline: "none !important",
-     color: "#ffffff"
- },
+  "& .super-app-theme--header": {
+    background: "rgba(24, 30, 47, 0.95)",
+    outline: "none !important",
+    color: "#ffffff",
+  },
+  "@media (max-width: 1024px)": {
+    maxWidth: "700px",
+  },
+  "@media (max-width: 768px)": {
+    maxWidth: "310px",
+  },
 };
 
 export const BillingSummaryBlock = () => {
@@ -66,16 +73,22 @@ export const BillingSummaryBlock = () => {
   );
 
   useEffect(() => {
-    if(data.length === 1) {
-      setSelectedRow(data[0])
+    if (data.length === 1) {
+      setSelectedRow(data[0]);
+    } else if (data.length > 1) {
+      const latestOrder = [...data].sort(
+        (a, b) =>
+          new Date(b.dateTime).getTime() - new Date(a.dateTime).getTime()
+      )[0];
+      setSelectedRow(latestOrder);
     }
-  }, [data])
+  }, [data]);
 
   return (
     <section className="bg-hubBackground">
-      <Box className="px-8 py-16 w-full">
+      <Box className="px-4 md:px-8 py-8 lg:py-16 w-full">
         <Box className="flex flex-col lg:flex-row gap-4 justify-center">
-          <Box className="flex flex-col w-auto h-auto bg-white rounded-[16px] drop-shadow-md">
+          <Box className="flex flex-col md:w-full lg:w-auto h-auto bg-white rounded-[16px] drop-shadow-md md:mx-auto lg:mx-0">
             <h4 className="h-[46px] flex items-center px-6 rounded-t-[16px] w-full text-white font-ptSansNarrow text-[20px] bg-gradient-to-r from-[#0F2A71] to-[#181E2F]">
               Order History
             </h4>
@@ -86,7 +99,7 @@ export const BillingSummaryBlock = () => {
                     ...col,
                     flex: undefined,
                     width: col.flex ? 200 : undefined,
-                    headerClassName: 'super-app-theme--header'
+                    headerClassName: "super-app-theme--header",
                   }))}
                   rows={data ?? []}
                   initPageSize={5}
@@ -99,7 +112,7 @@ export const BillingSummaryBlock = () => {
             </Box>
           </Box>
 
-          <Box className="flex flex-col gap-4 w-[323px]">
+          <Box className="flex flex-col md:flex-row lg:flex-col gap-4 w-auto lg:w-[323px]">
             <OrderSummaryBlock selectedRow={selectedRow} />
 
             <OrderDetailsBlock selectedRow={selectedRow} />
--- apps/web-customer/src/core/constant/BillingSummaryMock/BillingSummaryMock.ts
+++ apps/web-customer/src/core/constant/BillingSummaryMock/BillingSummaryMock.ts
@@ -11,8 +11,8 @@ interface BillingSummaryType {
 }
 
 export const billlingSummaryMockData: BillingSummaryType[] = [
-    { id: "1", orderId: 'RN23-7854861', dateTime: '08-15-2024 09:04:31', payment: 'Credit/Debit (Stripe)', receiptNumber: '854578654821', purchase: '23-Days standard Program (RN)', amount: '$230.00 USD', validity: '180 days (6 months) usage', promo: 'n/a' },
-    { id: "2", orderId: 'RN23-7854862', dateTime: '08-15-2024 09:04:32', payment: 'Debit (Stripe)', receiptNumber: '854578654822', purchase: '8-Days standard Program (RN)', amount: '$130.00 USD', validity: '90 days (3 months) usage', promo: 'n/a' },
-    { id: "3", orderId: 'RN23-7854863', dateTime: '08-15-2024 09:04:33', payment: 'Credit', receiptNumber: '854578654823', purchase: '23-Days standard Program (PN)', amount: '$330.00 USD', validity: '180 days (6 months) usage', promo: 'n/a' },
-    { id: "4", orderId: 'RN23-7854864', dateTime: '08-15-2024 09:04:34', payment: 'Debit', receiptNumber: '854578654824', purchase: '8-Days standard Program (PN)', amount: '$430.00 USD', validity: '90 days (3 months) usage', promo: 'n/a' },
+    { id: "1", orderId: 'RN23-7854864', dateTime: '08-16-2024 09:04:31', payment: 'Credit/Debit (Stripe)', receiptNumber: '854578654821', purchase: '23-Days standard Program (RN)', amount: '$230.00 USD', validity: '180 days (6 months) usage', promo: 'n/a' },
+    { id: "2", orderId: 'RN23-7854863', dateTime: '08-15-2024 09:04:32', payment: 'Debit (Stripe)', receiptNumber: '854578654822', purchase: '8-Days standard Program (RN)', amount: '$130.00 USD', validity: '90 days (3 months) usage', promo: 'n/a' },
+    { id: "3", orderId: 'RN23-7854862', dateTime: '08-15-2024 09:04:33', payment: 'Credit', receiptNumber: '854578654823', purchase: '23-Days standard Program (PN)', amount: '$330.00 USD', validity: '180 days (6 months) usage', promo: 'n/a' },
+    { id: "4", orderId: 'RN23-7854861', dateTime: '08-15-2024 09:04:34', payment: 'Debit', receiptNumber: '854578654824', purchase: '8-Days standard Program (PN)', amount: '$430.00 USD', validity: '90 days (3 months) usage', promo: 'n/a' },
   ];
