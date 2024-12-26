001bde0b2e74f07e493b7d6ee0e6b620ee23d02b
Lizter <lizters74@gmail.com>
2024-12-27 03:47:38 +0800

    @86eqyhrjf #AnnouncementAlert

---
+++ packages/core-library/components/Alert/AnnouncementAlert/AnnouncementAlert.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import Image from "next/image";
+import { CoreZigmaLogo } from "../../../assets";
+import { EvaIcon, IconButton } from "core-library/components";
+
+interface AnnouncementProps {
+  title: string;
+  content?: string;
+  Icon?: string;
+  isOpen: boolean;
+  bgColor?: string;
+  handleClose: () => void;
+}
+
+export const AnnouncementAlert: React.FC<AnnouncementProps> = ({
+  bgColor,
+  isOpen,
+  Icon,
+  title,
+  content,
+  handleClose,
+}) => {
+  return (
+    <>
+      {isOpen && (
+        <div
+          className="w-full h-auto bg-darkBlue flex fixed justify-between flex-wrap gap-10 p-3 z-[10000]"
+          style={{ backgroundColor: bgColor }}
+        >
+          <div>
+            <Image
+              alt="NCLEXANNOUNCEMENT"
+              src={CoreZigmaLogo || Icon}
+              width={50}
+            />
+          </div>
+          <div className="flex-1 text-white ">
+            <Typography variant="body1" fontFamily={"sans-serif"}>
+              {title}
+            </Typography>
+            <Typography variant="caption" fontFamily={"sans-serif"}>
+              {content}
+            </Typography>
+          </div>
+          <div>
+            <IconButton onClick={handleClose} className="w-24">
+              <EvaIcon
+                id="close-icon"
+                name="close"
+                fill="#fcf2f2"
+                aria-label="Close Modal"
+                ariaHidden
+              />
+            </IconButton>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
