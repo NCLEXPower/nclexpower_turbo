0c95c93d479987e5d74e89bffee84f49a324c41e
Christy Canete <christyacanete@gmail.com>
2024-12-30 15:59:37 +0800

    @86eqxm05r #Removed comments and changed #0F2A71 to darkBlue.

--- apps/web-customer/src/components/blocks/MaintenanceBlock/MaintenanceForm.tsx
+++ apps/web-customer/src/components/blocks/MaintenanceBlock/MaintenanceForm.tsx
@@ -28,7 +28,6 @@ export const MaintenanceForm: React.FC<Props> = ({ onSubmit }) => {
   return (
     <Container className='relative' maxWidth={false} sx={{ maxWidth: '1440px' }}>
 
-      {/* Background image */}
       <div className="absolute z-0 right-0 bottom-0">
         <Image
           src={MaintenanceModeBackground}
@@ -37,7 +36,6 @@ export const MaintenanceForm: React.FC<Props> = ({ onSubmit }) => {
         />
       </div>
 
-      {/* Construction worker image */}
       <div className="absolute z-1 right-0 bottom-0">
         <Image
           src={ConstructionWorkerImage}
@@ -46,21 +44,17 @@ export const MaintenanceForm: React.FC<Props> = ({ onSubmit }) => {
         />
       </div>
 
-      {/* Content */}
       <div className="flex flex-col items-center justify-center min-h-screen relative overflow-hidden">
         <div className="absolute top-[10%] left-1/2 transform -translate-x-1/2 sm:top-[18%] sm:left-10 sm:transform-none">
 
-          {/* Title */}
           <h1 className="max-w-[280px] sm:max-w-[400px] md:max-w-[600px] lg:max-w-[833px] xl:max-w-[1000px] 2xl:max-w-[1200px] font-ptSans font-bold text-[3rem] lg:text-[3.3rem] xl:text-7xl 2xl:text-8xl leading-snug md:leading-tight">
-            We’re Under <span className="text-[#0F2A71]">Maintenance</span>
+            We’re Under <span className="text-darkBlue">Maintenance</span>
           </h1>
 
-          {/* Description */}
-          <p className="max-w-full sm:max-w-[300px] md:max-w-[350px] xl:max-w-[450px] 2xl:max-w-[500px] mt-4 xl:mt-6 font-ptSans font-[400] text-[14px] sm:text-[14px] md:text-[16px] lg:text-[16.4px] xl:text-[21.5px] 2xl:text-[24.5px] leading-[24px] sm:leading-[27px] lg:leading-[28px] xl:leading-[36px] 2xl:leading-[40px] text-[#969696]">
+          <p className="max-w-full sm:max-w-[300px] md:max-w-[350px] xl:max-w-[450px] 2xl:max-w-[500px] mt-4 xl:mt-6 font-ptSans font-[400] text-[14px] sm:text-[14px] md:text-[16px] lg:text-[16.4px] xl:text-[21.5px] 2xl:text-[24.5px] leading-[24px] sm:leading-[27px] lg:leading-[28px] xl:leading-[36px] 2xl:leading-[40px] text-darkGray">
             Our site is down for improvements. Enter your email to be notified when we’re back!
           </p>
 
-          {/* Contact form */}
           <FormProvider {...form}>
             <div className="absolute w-full sm:max-w-[300px] md:max-w-[325px] lg:max-w-[290px] xl:max-w-[370px] 2xl:max-w-[400px] mt-6 lg:mt-8 xl:mt-10">
               <div className="flex flex-col gap-4 items-center justify-start w-full">
@@ -122,7 +116,6 @@ export const MaintenanceForm: React.FC<Props> = ({ onSubmit }) => {
             </div>
           </FormProvider>
 
-          {/* Contact us */}
           <div className="absolute w-full max-w-[280px] sm:max-w-[300px] lg:max-w-[500px] mt-52 lg:mt-56">
             <p
               className="text-[14px] xl:text-[16px] font-[400] leading-[24px] sm:leading-[27px] xl:leading-[30px] text-left"
@@ -131,7 +124,7 @@ export const MaintenanceForm: React.FC<Props> = ({ onSubmit }) => {
               If you need immediate assistance, feel free to contact us at{' '}
               <a
                 href="mailto:contact@nclexpower.com"
-                className="font-[700] underline text-[#0F2A71]"
+                className="font-[700] underline text-darkBlue"
               >
                 contact@nclexpower.com
               </a>
