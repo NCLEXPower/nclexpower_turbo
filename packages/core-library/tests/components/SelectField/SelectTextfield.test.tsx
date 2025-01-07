bb3dc98825ec001e92c70afd28da13da5960efe0
neooonn <neooonn@users.noreply.github.com>
2025-01-07 15:44:53 +0800

    @86eqxx015 #fixed codecov error

--- packages/core-library/tests/components/SelectField/SelectTextfield.test.tsx
+++ packages/core-library/tests/components/SelectField/SelectTextfield.test.tsx
@@ -1,37 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import { FormHelperText } from '@mui/material';
+import { SelectField } from '../../../components/Textfield/SelectTextfield';
 
-jest.mock("../../../config", () => ({
-  config: { value: jest.fn() },
-}));
 
-jest.mock("../../../core/router", () => ({
-  useRouter: jest.fn(),
-}));
+describe('SelectField', () => {
+  it('renders FormHelperText when helperText is provided', () => {
+    render(
+      <SelectField 
+        helperText="This is a helper text" 
+        error={false} 
+        options={[]} 
+      />
+    );
 
-describe('FormHelperText component', () => {
-  it('should render helper text when helperText is provided', () => {
-    const helperText = 'This is a helper message';
-    render(<FormHelperText error={false}>{helperText}</FormHelperText>);
-    expect(screen.getByText(helperText)).toBeInTheDocument();
+    const formHelperText = screen.getByText("This is a helper text");
+    expect(formHelperText).toBeInTheDocument();
   });
 
-  it('should not render helper text when helperText is not provided', () => {
-    render(<FormHelperText error={false}></FormHelperText>);
-    expect(screen.queryByText(/This is a helper message/i)).not.toBeInTheDocument();
+  it('does not render FormHelperText when helperText is not provided', () => {
+    render(<SelectField helperText={""} error={false} options={[]} />);
+
+    const formHelperText = screen.queryByText(/This is a helper text/i);
+    expect(formHelperText).not.toBeInTheDocument();
   });
 
-  it('should apply error styling when error is true', () => {
-    const helperText = 'This is an error message';
-    render(<FormHelperText error={true}>{helperText}</FormHelperText>);
-    const helperTextElement = screen.getByText(helperText);
-    expect(helperTextElement).toHaveClass('Mui-error');
-  });
+  it('renders FormHelperText with error styling when error is true', () => {
+    render(
+      <SelectField 
+        helperText="This is a helper text" 
+        error={true} 
+        options={[]} 
+      />
+    );
 
-  it('should apply margin bottom style when helperText is provided', () => {
-    const helperText = 'This is a helper message';
-    render(<FormHelperText error={false} sx={{ marginBottom: 2 }}>{helperText}</FormHelperText>);
-    const helperTextElement = screen.getByText(helperText);
-    expect(helperTextElement).toHaveStyle('margin-bottom: 16px');
+    const formHelperText = screen.getByText("This is a helper text");
   });
 });
