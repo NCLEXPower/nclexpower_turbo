ef7215d92e7c3ff3ce54cc11cfccd297e181626a
Brian James (BJ) <brianjamesdlcz@gmail.com>
2025-02-08 23:28:56 +0800

    @86erfhg1j #Added more unit testing on direct changes

--- packages/core-library/tests/components/Blocks/CasetStudy/MCQNoGroup.test.tsx
+++ packages/core-library/tests/components/Blocks/CasetStudy/MCQNoGroup.test.tsx
@@ -98,40 +98,17 @@ describe("MCQ No Group", () => {
     );
   });
 
-  // it("should initialize columns and rows if they are empty", () => {
-  //   const setValueMock = jest.fn();
-  //   jest.mock("react-hook-form", () => ({
-  //     useFormContext: jest.fn(() => ({
-  //       watch: jest.fn(),
-  //       control: jest.fn(),
-  //       setValue: setValueMock,
-  //       getValues: jest.fn(() => ({
-  //         questionnaires: [{ columns: [], rows: [] }],
-  //       })),
-  //     })),
-  //     useFieldArray: jest.fn(() => ({
-  //       append: jest.fn(),
-  //       remove: jest.fn(),
-  //     })),
-  //   }));
+  it("should call handleRemoveColumnHeaders when remove column headers button is clicked", () => {
+    render(<MCQNoGroup questionIndex={1} />);
+    const removeColumnHeadersButton = screen.getByTestId("remove-column-headers");
+    removeColumnHeadersButton.click();
+    expect(setValueMock).toHaveBeenCalled();
+  });
 
-  //   render(<MCQNoGroup questionIndex={1} />);
-  //   expect(setValueMock).toHaveBeenCalledWith(
-  //     "questionnaires.1.columns", [{ "label": "" }, { "label": "" }, { "label": "" }]
-  //   );
-  //   expect(setValueMock).toHaveBeenCalledWith(
-  //     "questionnaires.1.rows",
-  //     [
-  //       {
-  //         rowTitle: "",
-  //         rowId: 0,
-  //         choices: [
-  //           { value: false, choiceId: 0 },
-  //           { value: false, choiceId: 1 },
-  //         ],
-  //       }
-  //     ]
-  //     Array(1).fill(initMCQRow)
-  //   );
-  // });
+  it("should call handleRemoveRow when remove row button is clicked", () => {
+    render(<MCQNoGroup questionIndex={1} />);
+    const removeRowTableButton = screen.getByTestId("remove-row");
+    removeRowTableButton.click();
+    expect(setValueMock).toHaveBeenCalled();
+  });
 });
