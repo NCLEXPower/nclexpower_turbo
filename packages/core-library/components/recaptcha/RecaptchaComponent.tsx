5248e57bfdf3185f4c9313ece40c75abf4841bd5
mrkmartin <martinmarkpaulo12@gmail.com>
2025-02-13 14:41:16 +0800

    add loader in recaptcha

--- packages/core-library/components/recaptcha/RecaptchaComponent.tsx
+++ packages/core-library/components/recaptcha/RecaptchaComponent.tsx
@@ -1,15 +1,33 @@
-import React, { forwardRef } from "react";
-import ReCAPTCHA, { ReCAPTCHAProps } from "react-google-recaptcha";
+import React, { forwardRef, useEffect, useState } from 'react';
+import ReCAPTCHA, { ReCAPTCHAProps } from 'react-google-recaptcha';
+import { ComponentLoader } from '../ComponentLoader';
+import { Box } from '@mui/material';
 
 export const RecaptchaComponent = forwardRef<ReCAPTCHA, ReCAPTCHAProps>(
   (props, ref) => {
+    const [isRecaptchaVisible, setIsRecaptchaVisible] = useState(false);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const recaptchaFrame = document.querySelector(
+          "iframe[title='reCAPTCHA']"
+        );
+        if (recaptchaFrame) {
+          setIsRecaptchaVisible(true);
+          clearInterval(interval);
+        }
+      }, 500);
+
+      return () => clearInterval(interval);
+    }, []);
+
     return (
-      <div style={{ marginTop: "10px" }}>
-        <ReCAPTCHA
-          {...props}
-          ref={ref as React.RefObject<ReCAPTCHA>}
-        />
-      </div>
+      <Box mt={2} position='relative'>
+        {!isRecaptchaVisible && <ComponentLoader />}
+        <div style={{ visibility: isRecaptchaVisible ? 'visible' : 'hidden' }}>
+          <ReCAPTCHA {...props} ref={ref as React.RefObject<ReCAPTCHA>} />
+        </div>
+      </Box>
     );
   }
 );
