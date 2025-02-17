9e82952d98195b65099496df3e149770150cd274
mrkmartin <martinmarkpaulo12@gmail.com>
2025-02-17 15:31:27 +0800

    @86erh246f #Added skeleton loader in recaptcha

--- packages/core-library/components/recaptcha/RecaptchaComponent.tsx
+++ packages/core-library/components/recaptcha/RecaptchaComponent.tsx
@@ -1,33 +1,67 @@
 import React, { forwardRef, useEffect, useState } from 'react';
 import ReCAPTCHA, { ReCAPTCHAProps } from 'react-google-recaptcha';
-import { ComponentLoader } from '../ComponentLoader';
-import { Box } from '@mui/material';
+import { AnimatedBoxSkeleton } from '../AnimatedBoxSkeleton/AnimatedSkeletonBox';
+
+declare global {
+  interface Window {
+    grecaptcha?: {
+      render: (...args: any[]) => void;
+      getResponse: (...args: any[]) => string;
+      reset: (...args: any[]) => void;
+      ready: (callback: () => void) => void;
+    };
+  }
+}
 
 export const RecaptchaComponent = forwardRef<ReCAPTCHA, ReCAPTCHAProps>(
   (props, ref) => {
-    const [isRecaptchaVisible, setIsRecaptchaVisible] = useState(false);
+    const [isRecaptchaLoaded, setIsRecaptchaLoaded] = useState(false);
+    const [isScriptLoaded, setIsScriptLoaded] = useState(false);
 
     useEffect(() => {
-      const interval = setInterval(() => {
-        const recaptchaFrame = document.querySelector(
-          "iframe[title='reCAPTCHA']"
-        );
-        if (recaptchaFrame) {
-          setIsRecaptchaVisible(true);
-          clearInterval(interval);
-        }
-      }, 500);
-
-      return () => clearInterval(interval);
+      if (window.grecaptcha) {
+        window.grecaptcha.ready(() => {
+          setIsRecaptchaLoaded(true);
+        });
+      } else {
+        const script = document.createElement('script');
+        script.src = 'https://www.google.com/recaptcha/api.js';
+        script.async = true;
+        script.onload = () => setIsScriptLoaded(true);
+        document.body.appendChild(script);
+      }
     }, []);
 
+    useEffect(() => {
+      if (isScriptLoaded) {
+        const checkRecaptcha = setInterval(() => {
+          if (window.grecaptcha) {
+            window.grecaptcha.ready(() => {
+              setIsRecaptchaLoaded(true);
+              clearInterval(checkRecaptcha);
+            });
+          }
+        }, 500);
+
+        return () => clearInterval(checkRecaptcha);
+      }
+    }, [isScriptLoaded]);
+
     return (
-      <Box mt={2} position='relative'>
-        {!isRecaptchaVisible && <ComponentLoader />}
-        <div style={{ visibility: isRecaptchaVisible ? 'visible' : 'hidden' }}>
+      <div style={{ marginTop: '10px', position: 'relative' }}>
+        {!isRecaptchaLoaded && (
+          <AnimatedBoxSkeleton
+            light
+            data-testid='skeleton-loader'
+            style={{ width: '302px', height: '76px' }}
+          >
+            Loading ...
+          </AnimatedBoxSkeleton>
+        )}
+        {isRecaptchaLoaded && (
           <ReCAPTCHA {...props} ref={ref as React.RefObject<ReCAPTCHA>} />
-        </div>
-      </Box>
+        )}
+      </div>
     );
   }
 );
