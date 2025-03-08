4cb79d862c8204c5f3f1d61eac33bd4c1f3781c3
JezChristian <bacasnojezreel@gmail.com>
2025-03-09 01:26:39 +0800

    #86errqbbf @Component-Loader

--- packages/core-library/components/ComponentState/ComponentState.tsx
+++ packages/core-library/components/ComponentState/ComponentState.tsx
@@ -1,77 +1,100 @@
-import Image from 'next/image'
-import { EmptyStateImage, ErrorStateImage } from '../../assets'
-import { Box } from '@mui/material'
-import { ComponentLoader } from '../ComponentLoader'
-import { PropsWithChildren, useEffect, useState } from 'react'
+import Image from "next/image";
+import { EmptyStateImage, ErrorStateImage } from "../../assets";
+import { Box } from "@mui/material";
+import { ComponentLoader } from "../ComponentLoader";
+import { PropsWithChildren, useEffect, useState } from "react";
 
+export const ComponentState: React.FC<PropsWithChildren<unknown | any>> = ({
+  data,
+  isSuccess,
+  isError,
+  isLoading,
+  children,
+}) => {
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
 
-export const ComponentState: React.FC<PropsWithChildren<unknown | any>> = ({ data, isSuccess, isError, isLoading, children }) => {
-    const [isEmpty, setIsEmpty] = useState<boolean>(false)
-
-
-    useEffect(() => {
-        if ((data.result?.length === 0 || data.result == null) && isSuccess) {
-            setIsEmpty(true);
-        } else {
-            setIsEmpty(false);
-        }
-    }, [data.result, data.status]);
-
-    if (isSuccess && !isEmpty) {
-        return <>{children}</>
+  useEffect(() => {
+    if (
+      !data.result ||
+      (Array.isArray(data.result) && data.result.length === 0)
+    ) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
     }
+  }, [data]);
 
+  if (isSuccess && !isEmpty) {
+    return <>{children}</>;
+  }
+  return (
+    <Box
+      data-testid="component-states-id"
+      sx={{
+        display: "flex",
+        backgroundColor: "white",
+        borderRadius: "15px",
+        minWidth: "250px",
+        width: "350px",
+        height: "400px",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        boxShadow: 2,
+      }}
+    >
+      <StateStatus isError={isError} isLoading={isLoading} isEmpty={isEmpty} />
+    </Box>
+  );
+};
+
+export const StateStatus = ({
+  isError,
+  isLoading,
+  isEmpty,
+}: {
+  isError: boolean;
+  isLoading: boolean;
+  isEmpty: boolean;
+}) => {
+  if (isLoading) {
     return (
-        <Box data-testid="component-states-id" sx={{
-            display: 'flex',
-            backgroundColor: 'white',
-            borderRadius: '15px',
-            minWidth: '250px',
-            width: "350px",
-            height: '400px',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-            boxShadow: 2
-        }}>
-            <StateStatus isError={isError} isLoading={isLoading} isEmpty={isEmpty} />
+      <Box data-testid="component-is-loading-id">
+        <ComponentLoader disableMarginBottom />
+        <Box sx={{ bgcolor: "Background" }}>
+          <p className="h-fit font-bold leading-3 w-full">Loading</p>
+          <p className="h-fit text-[.8rem] leading-3 -mt-3 w-full">
+            Please Wait...
+          </p>
         </Box>
-    )
-
-
-}
-
-const StateStatus = ({ isError, isLoading, isEmpty }: { isError: boolean, isLoading: boolean, isEmpty: boolean }) => {
-    if (isEmpty) {
-        return (<>
-            <Image src={EmptyStateImage} alt='Empty Data' />
-            <Box data-testid="component-is-empty-id">
-                <p className='h-fit font-bold leading-3 w-full'>Empty!</p>
-                <p className='h-fit text-[.8rem] leading-3 -mt-3 w-full'>No Data Available</p>
-            </Box>
-        </>)
-    }
-    if (isError) {
-        return (
-            <>
-                <Image src={ErrorStateImage} alt='Error Occured' />
-                <Box data-testid="component-is-error-id">
-                    <p className='h-fit font-bold leading-3 w-full'>Error!</p>
-                    <p className='h-fit text-[.8rem] leading-3 -mt-3 w-full'>Something went wrong</p>
-                </Box>
-            </>
-        )
-    }
-    if (isLoading) {
-        return (
-            <Box data-testid="component-is-loading-id">
-                <ComponentLoader disableMarginBottom />
-                <Box sx={{ bgcolor: 'Background' }}>
-                    <p className='h-fit font-bold leading-3 w-full'>Loading</p>
-                    <p className='h-fit text-[.8rem] leading-3 -mt-3 w-full'>Please Wait...</p>
-                </Box>
-            </Box>)
-    }
-
-}
+      </Box>
+    );
+  }
+  if (isEmpty) {
+    return (
+      <>
+        <Image src={EmptyStateImage} alt="Empty Data" />
+        <Box data-testid="component-is-empty-id">
+          <p className="h-fit font-bold leading-3 w-full">Empty!</p>
+          <p className="h-fit text-[.8rem] leading-3 -mt-3 w-full">
+            No Data Available
+          </p>
+        </Box>
+      </>
+    );
+  }
+  if (isError) {
+    return (
+      <>
+        <Image src={ErrorStateImage} alt="Error Occured" />
+        <Box data-testid="component-is-error-id">
+          <p className="h-fit font-bold leading-3 w-full">Error!</p>
+          <p className="h-fit text-[.8rem] leading-3 -mt-3 w-full">
+            Something went wrong
+          </p>
+        </Box>
+      </>
+    );
+  }
+};
