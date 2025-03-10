023326c56669a0beb432b3c564d84002d3af23f6
mrkmartin <martinmarkpaulo12@gmail.com>
2025-03-10 16:59:46 +0800

    @86ernnywp #Implemented downloading pdf of Terms of Service and Privacy Policy

--- apps/web-customer/src/components/register-line/steps/content/AgreementRegistration.tsx
+++ apps/web-customer/src/components/register-line/steps/content/AgreementRegistration.tsx
@@ -4,19 +4,20 @@ import {
   ControlledCheckbox,
   EvaIcon,
   IconButton,
-  RecaptchaComponent
+  RecaptchaComponent,
 } from 'core-library/components';
 import {
   RegistrationAtom,
   RegistrationFormType,
-  registrationSchema
+  registrationSchema,
 } from './validation';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from "@hookform/resolvers/yup";
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Typography } from '@mui/material';
 import { useAtom } from 'jotai';
 import { useRegistrationWalkthroughFormContext } from '../../RegistrationWalkthroughContext';
+import { useDownloadPDF } from '../../../../../../../packages/core-library/hooks/useDownloadPDF';
 
 interface Props {
   nextStep(values: Partial<RegistrationFormType>): void;
@@ -32,33 +33,29 @@ export const AgreementRegistration: React.FC<Props> = ({
   previousStep,
   previous,
   resetStep,
-  reset
+  reset,
 }) => {
   const [registrationDetails] = useAtom(RegistrationAtom);
-  const {
-    onSubmit,
-    isLoading,
-    recaptchaRef,
-    siteKey
-  } = useRegistrationWalkthroughFormContext();
+  const { onSubmit, isLoading, recaptchaRef, siteKey } =
+    useRegistrationWalkthroughFormContext();
   const [captchaToken, setCaptchaToken] = useState<string | null>(null);
   const form = useForm<RegistrationFormType>({
-    mode: "onChange",
+    mode: 'onChange',
     resolver: yupResolver(registrationSchema),
     defaultValues: {
-      ...registrationDetails
-    }
+      ...registrationDetails,
+    },
   });
 
   const { control, formState, handleSubmit, watch } = form;
   const { isDirty, isValid } = formState;
 
-  const agreement = watch("termsofservice");
+  const agreement = watch('termsofservice');
 
   async function onFormSubmit(values: RegistrationFormType) {
     const data = { ...registrationDetails, ...values };
     if (!captchaToken) {
-      console.error("reCAPTCHA verification failed.");
+      console.error('reCAPTCHA verification failed.');
       return;
     }
 
@@ -73,64 +70,104 @@ export const AgreementRegistration: React.FC<Props> = ({
     reset();
   };
 
+  const { downloadPdf } = useDownloadPDF();
+
+  const handleDownloadPDF = (policyType: number) => () => {
+    downloadPdf(policyType);
+  };
+
   return (
     <div>
-      <Box className="flex flex-col">
-        <Box className="flex items-center">
-        <ControlledCheckbox
-          control={control}
-          name="termsofservice"
-        />
-        <Typography sx={{ fontFamily: "PT Sans Narrow", fontSize: "18px", fontStyle: "normal", fontWeight: 400, lineHeight: "2.25rem", color: "#0F2A71", marginLeft: -3 }}>
-          I accept <span className='text-darkBlue font-bold underline'>Terms of Service</span> and <span className='text-darkBlue font-bold underline'>Privacy Policy</span>
-        </Typography>
+      <Box className='flex flex-col'>
+        <Box className='flex items-center'>
+          <ControlledCheckbox control={control} name='termsofservice' />
+          <Typography
+            sx={{
+              fontFamily: 'PT Sans Narrow',
+              fontSize: '18px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: '2.25rem',
+              color: '#0F2A71',
+              marginLeft: -3,
+            }}
+          >
+            I accept{' '}
+            <span className='text-darkBlue font-bold underline'>
+              <a
+                href='#'
+                className='!bg-transparent !focus:bg-transparent !active:bg-transparent 
+             !ring-0 !focus:outline-none !border-transparent'
+                onClick={handleDownloadPDF(0)}
+              >
+                Terms of Service
+              </a>
+            </span>{' '}
+            and{' '}
+            <span className='text-darkBlue font-bold underline'>
+              <a
+                href='#'
+                className='!bg-transparent !focus:bg-transparent !active:bg-transparent 
+             !ring-0 !focus:outline-none !border-transparent'
+                onClick={handleDownloadPDF(1)}
+              >
+                Privacy Policy
+              </a>
+            </span>
+          </Typography>
         </Box>
         {!agreement && (
-           <Typography sx={{ color: "#bd321c", fontFamily: "PT Sans Narrow", fontSize: "14px", fontStyle: "normal", fontWeight: 400}}>
+          <Typography
+            sx={{
+              color: '#bd321c',
+              fontFamily: 'PT Sans Narrow',
+              fontSize: '14px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+            }}
+          >
             You must read and accept the Terms of Service and Privacy Policy
           </Typography>
         )}
       </Box>
-      <div className="flex items-center w-full gap-2">
+      <div className='flex items-center w-full gap-2'>
         <IconButton
           sx={{
-            backgroundColor: "#0F2A71",
-            borderRadius: "10px",
-            padding: "12px",
-            "&:hover": {
-              backgroundColor: "#0F2A7195",
-            }
+            backgroundColor: '#0F2A71',
+            borderRadius: '10px',
+            padding: '12px',
+            '&:hover': {
+              backgroundColor: '#0F2A7195',
+            },
           }}
           onClick={handlePrevious}
         >
           <EvaIcon
-            name="arrow-back-outline"
-            fill="#fff"
+            name='arrow-back-outline'
+            fill='#fff'
             width={25}
             height={25}
             ariaHidden
           />
         </IconButton>
         <Button
-          disabled={
-            !isDirty || !isValid || !captchaToken || !agreement
-          }
+          disabled={!isDirty || !isValid || !captchaToken || !agreement}
           loading={isLoading}
-          variant="contained"
+          variant='contained'
           sx={{
-            width: "85%",
+            width: '85%',
             px: 4,
             py: 2,
-            backgroundColor: "#0F2A71",
-            borderRadius: "10px",
-            "&:hover": {
-              backgroundColor: "#00173F",
+            backgroundColor: '#0F2A71',
+            borderRadius: '10px',
+            '&:hover': {
+              backgroundColor: '#00173F',
             },
-            marginY: "10px",
+            marginY: '10px',
           }}
           onClick={handleSubmit(onFormSubmit)}
         >
-          <span className="font-ptSans font-bold">Create Account</span>
+          <span className='font-ptSans font-bold'>Create Account</span>
         </Button>
       </div>
 
