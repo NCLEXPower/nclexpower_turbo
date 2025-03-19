d2e4fa6daa1d16bba423f000b0f559f1eb38f656
StevenMadali <stevenmadali17@gmail.com>
2025-03-19 14:50:10 +0800

    Fix Conflict

--- packages/core-library/system/app/internal/blocks/Hub/ContactUs/ContactUsManagement/ContactUsManagementBlock.tsx
+++ packages/core-library/system/app/internal/blocks/Hub/ContactUs/ContactUsManagement/ContactUsManagementBlock.tsx
@@ -15,7 +15,6 @@ import { useExecuteToast } from "../../../../../../../contexts";
 export const ContactUsManagementBlock: React.FC = () => {
     const { open, close, props } = useModal();
     const { showToast } = useExecuteToast();
-
     const getContacts = useApi((api) => api.webbackoffice.getAllContacts());
     const deleteContact = useApiCallback((api, id: string) => api.webbackoffice.deleteContact(id));
 
@@ -100,64 +99,18 @@ export const ContactUsManagementBlock: React.FC = () => {
             severity="info"
             title="Contact Us Management"
             description="Access a comprehensive list of all internal users along with their details for streamlined tracking and effective management."
-=======
-  const getContacts = useApi((api) => api.webbackoffice.getAllContacts());
-
-  const contacts =
-    (getContacts.result?.data as unknown as ContactResponseType[]) ?? [];
-
-  const { columns } = useColumns({
-    columns: [
-      {
-        field: "name",
-        headerName: "Name",
-        minWidth: 250,
-        flex: 1,
-      },
-      {
-        field: "email",
-        headerName: "Email",
-        flex: 1,
-      },
-      {
-        field: "phone",
-        headerName: "Phone",
-        flex: 1,
-      },
-      {
-        field: "message",
-        headerName: "Message",
-        flex: 1,
-      },
-      {
-        field: "createdAt",
-        headerName: "Created At",
-        flex: 1,
-        sortable: true,
-        minWidth: 200,
-      },
-    ],
-  });
-
-  return (
-    <Box>
-      <Container>
-        <Alert
-          severity="info"
-          title="Contact Us Management"
-          description="Access a comprehensive list of all internal users along with their details for streamlined tracking and effective management."
-        />
-        <Card sx={{ mt: 5, width: "100%", padding: 4 }} elevation={5}>
-          <DataGrid
-            columns={columns}
-            initPageSize={10}
-            rows={contacts}
-            isLoading={getContacts.loading}
-            data-testid="data-grid"
-            disableColumnResize
           />
-        </Card>
-      </Container>
-    </Box>
-  );
+          <Card sx={{ mt: 5, width: "100%", padding: 4 }} elevation={5}>
+            <DataGrid
+              columns={columns}
+              initPageSize={10}
+              rows={contacts}
+              isLoading={getContacts.loading}
+              data-testid="data-grid"
+              disableColumnResize
+            />
+          </Card>
+        </Container>
+      </Box>
+    );
 };
