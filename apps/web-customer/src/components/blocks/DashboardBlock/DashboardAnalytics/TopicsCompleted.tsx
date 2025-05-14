bec010f521c0638ae80cd04fd91c2c45410b24f6
mrkmartin <martinmarkpaulo12@gmail.com>
2025-05-14 23:35:13 +0800

    @86erzrt7z #Move the helper function inside the TopicsCompleted component file

--- apps/web-customer/src/components/blocks/DashboardBlock/DashboardAnalytics/TopicsCompleted.tsx
+++ apps/web-customer/src/components/blocks/DashboardBlock/DashboardAnalytics/TopicsCompleted.tsx
@@ -9,8 +9,51 @@ import { analytics } from "./DashboardMock";
 import { Box, Typography } from "@mui/material";
 import Divider from "core-library/components/Divider/Divider";
 import { useResolution } from "core-library/hooks";
-import { getLayoutConfig } from "@/utils/getLayoutConfig";
 import { ProgressRing } from "core-library/components";
+
+const getLayoutConfig = (isMobile: boolean, index: number) => {
+  const isFirst = index === 0;
+  const isSecond = index === 1;
+
+  return {
+    top: !isMobile ? (isFirst ? 40 : isSecond ? 20 : 50) : isFirst ? 65 : 50,
+
+    left: !isMobile ? (isFirst ? 35 : isSecond ? 15 : 20) : isFirst ? 35 : 20,
+
+    progressSize: !isMobile
+      ? isFirst
+        ? 90
+        : isSecond
+          ? 130
+          : 80
+      : isFirst
+        ? 50
+        : 80,
+
+    labelTop: !isMobile
+      ? isFirst
+        ? "35%"
+        : isSecond
+          ? "40%"
+          : "30%"
+      : isFirst
+        ? "17%"
+        : "30%",
+
+    labelLeft: !isMobile
+      ? isFirst
+        ? "42%"
+        : isSecond
+          ? "72%"
+          : "63%"
+      : isFirst
+        ? "26%"
+        : "63%",
+
+    progressColor: index % 2 === 0 ? "#0F2A71" : "#181E2F",
+    dividerHeight: !isMobile ? "80px" : "60px",
+  };
+};
 
 export const TopicsCompleted = () => {
   const { isMobile } = useResolution();
