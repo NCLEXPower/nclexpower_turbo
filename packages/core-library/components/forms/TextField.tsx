4bea7b6f7b38123a93443ae5b1780f0034d9f8ed
Khim Rata <khimrata11@gmail.com>
2025-06-06 02:24:43 +0800

    @86etk5z89 #fix: restore accidentally removed imports in components

--- packages/core-library/components/forms/TextField.tsx
+++ packages/core-library/components/forms/TextField.tsx
@@ -103,8 +103,7 @@ export const TextFieldComponent = <T extends object>({
   return (
     <Grid container spacing={2} direction="column">
       <Grid item>
-        {fieldState?.error?.message &&
-        (fieldState?.isDirty || fieldState?.isTouched) ? (
+        {fieldState?.error?.message ? (
           <FormHelperText error>{fieldState?.error?.message}</FormHelperText>
         ) : (
           label !== null && (
